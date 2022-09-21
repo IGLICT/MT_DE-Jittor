315886d9b78d9ada9b6ba3471745197a43dc66c4
sunyangtian <1421214992@qq.com>
2022-09-21 09:42:23 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -32,3 +32,34 @@ if [ ! -d $datasetname ]; then
   ln -s "${datadir}/${srcaim}/img_obj_smooth_512_norm" "${codedir}/${datasetname}/test_src_densepose"
   ln -s "${datadir}/${srcaim}/openpose_img_512_norm"   "${codedir}/${datasetname}/test_src_openpose"
 fi
+
+# run
+if [ $do_test == 0 ]; then
+python train.py \
+	--name $trainname \
+	--nThreads 2 \
+	--dataroot $datasetname \
+	--n_blocks_global 6 \
+	--ngf 32 \
+	--ndf 32 \
+	--norm instance \
+	--batchSize 1 \
+	--resize_or_crop scale_width \
+	--loadSize 512 \
+	--fineSize 512 \
+	--save_latest_freq 5000 \
+	--display_freq 10 \
+	--print_freq 10 \
+	--no_flip \
+	--tf_log \
+	--no_ps_loss \
+	--num_of_frame 2 \
+	--input_type 0 \
+	--usemulti \
+	--gpu_ids $gpu_ids
+#	--continue_train
+	# --resize_or_crop scale_width \
+	# --continue_train
+	#--lambda_gan_feat 100 \
+	# --no_ganFeat_loss 
+fi
